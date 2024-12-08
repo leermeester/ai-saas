ffd5abab4ba96d058bfa414fc773ea72f220b5b5
leermeester <dirkjanvanveen@gmail.com>
2024-12-08 22:03:56 +0900

    feat/landingpage start

---
+++ components/landing-navbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Montserrat } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import { useAuth } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+
+const font = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
